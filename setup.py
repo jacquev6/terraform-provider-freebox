c9694f728e8920d3e86ff32e5d9dfeafb2d12107
Vincent Jacques <vincent@vincent-jacques.net>
2020-09-24 10:11:19 +0200

    (Try to) Publish on PyPI

--- setup.py
+++ setup.py
@@ -7,6 +7,23 @@ version = "0.0.1"
 setuptools.setup(
     name="terraform-provider-freebox",
     version=version,
+    description="Terraform provider to configure Freebox xDSL/FTTH modems",
+    long_description=open("README.rst").read(),
+
+    author="Vincent Jacques",
+    author_email="vincent@vincent-jacques.net",
+    url="http://jacquev6.github.io/terraform-provider-freebox",
+    license="MIT",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+
     install_requires=open("requirements.txt").readlines(),
     packages=setuptools.find_packages(),
     entry_points = {
